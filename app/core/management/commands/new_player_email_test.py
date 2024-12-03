8d1055c399443c4932595560111f1b341df7ab07
moviefan322 <philman202@yahoo.com>
2024-12-03 11:12:38 -0500

    deletes user after email test

--- app/core/management/commands/new_player_email_test.py
+++ app/core/management/commands/new_player_email_test.py
@@ -22,3 +22,7 @@ class Command(BaseCommand):
         else:
             self.stdout.write(self.style.WARNING(
                 f"Player with email {email} already exists."))
+        
+        # delete email if Player/User exists
+        Player.objects.filter(email=email).delete()
+        User.objects.filter(email=email).delete()
